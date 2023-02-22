39c060ca75f328b2bd6da1356cb31403bf97b960
ryanherren <ryanherren48@gmail.com>
2023-02-22 03:10:04 -0600

    implemented h2 manhattan distance heuristic

--- search.py
+++ search.py
@@ -491,9 +491,163 @@ class EightPuzzle(Problem):
     def h(self, node):
         """ Return the heuristic value for a given state. Default heuristic function used is 
         h(n) = number of misplaced tiles """
-
         return sum(s != g for (s, g) in zip(node.state, self.goal))
 
+
+class EightPuzzle2(Problem):
+    """ The problem of sliding tiles numbered from 1 to 8 on a 3x3 board, where one of the
+    squares is a blank. A state is represented as a tuple of length 9, where  element at
+    index i represents the tile number  at index i (0 if it's an empty square) """
+
+    def __init__(self, initial, goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)):
+        """ Define goal state and initialize a problem """
+        super().__init__(initial, goal)
+
+    def find_blank_square(self, state):
+        """Return the index of the blank square in a given state"""
+
+        return state.index(0)
+
+    def actions(self, state):
+        """ Return the actions that can be executed in the given state.
+        The result would be a list, since there are only four possible actions
+        in any given state of the environment """
+
+        possible_actions = ['D', 'U', 'R', 'L']
+        index_blank_square = self.find_blank_square(state)
+
+        if index_blank_square % 3 == 0:
+            possible_actions.remove('R')
+        if index_blank_square < 3:
+            possible_actions.remove('D')
+        if index_blank_square % 3 == 2:
+            possible_actions.remove('L')
+        if index_blank_square > 5:
+            possible_actions.remove('U')
+
+        return possible_actions
+
+    def result(self, state, action):
+        """ Given state and action, return a new state that is the result of the action.
+        Action is assumed to be a valid action in the state """
+
+        # blank is the index of the blank square
+        blank = self.find_blank_square(state)
+        new_state = list(state)
+
+        delta = {'D': -3, 'U': 3, 'R': -1, 'L': 1}
+        neighbor = blank + delta[action]
+        new_state[blank], new_state[neighbor] = new_state[neighbor], new_state[blank]
+
+        return tuple(new_state)
+
+    def goal_test(self, state):
+        """ Given a state, return True if state is a goal state or False, otherwise """
+
+        return state == self.goal
+
+    def check_solvability(self, state):
+        """ Checks if the given state is solvable """
+
+        inversion = 0
+        for i in range(len(state)):
+            for j in range(i + 1, len(state)):
+                if (state[i] > state[j]) and state[i] != 0 and state[j] != 0:
+                    inversion += 1
+
+        return inversion % 2 == 0
+
+    def h(self, node):
+        """ Return the heuristic value for a given state."""
+
+        def find_blank(node):
+            num = 0
+            for i in node.state:
+                num = num + 1
+                if (i == 0):
+                    break;
+            return num;
+
+        num = find_blank(node)
+        # Manhattan Heuristic Function (computations by hand, hardcoded)
+        if num in (8, 6):
+            return 1
+        elif num in (3, 5, 6):
+            return 2
+        elif num in (2, 4):
+            return 3
+        elif num == 1:
+            return 4
+        else:
+            return 0
+
+
+class EightPuzzle3(Problem):
+    """ The problem of sliding tiles numbered from 1 to 8 on a 3x3 board, where one of the
+    squares is a blank. A state is represented as a tuple of length 9, where  element at
+    index i represents the tile number  at index i (0 if it's an empty square) """
+
+    def __init__(self, initial, goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)):
+        """ Define goal state and initialize a problem """
+        super().__init__(initial, goal)
+
+    def find_blank_square(self, state):
+        """Return the index of the blank square in a given state"""
+
+        return state.index(0)
+
+    def actions(self, state):
+        """ Return the actions that can be executed in the given state.
+        The result would be a list, since there are only four possible actions
+        in any given state of the environment """
+
+        possible_actions = ['D', 'U', 'R', 'L']
+        index_blank_square = self.find_blank_square(state)
+
+        if index_blank_square % 3 == 0:
+            possible_actions.remove('R')
+        if index_blank_square < 3:
+            possible_actions.remove('D')
+        if index_blank_square % 3 == 2:
+            possible_actions.remove('L')
+        if index_blank_square > 5:
+            possible_actions.remove('U')
+
+        return possible_actions
+
+    def result(self, state, action):
+        """ Given state and action, return a new state that is the result of the action.
+        Action is assumed to be a valid action in the state """
+
+        # blank is the index of the blank square
+        blank = self.find_blank_square(state)
+        new_state = list(state)
+
+        delta = {'D': -3, 'U': 3, 'R': -1, 'L': 1}
+        neighbor = blank + delta[action]
+        new_state[blank], new_state[neighbor] = new_state[neighbor], new_state[blank]
+
+        return tuple(new_state)
+
+    def goal_test(self, state):
+        """ Given a state, return True if state is a goal state or False, otherwise """
+
+        return state == self.goal
+
+    def check_solvability(self, state):
+        """ Checks if the given state is solvable """
+
+        inversion = 0
+        for i in range(len(state)):
+            for j in range(i + 1, len(state)):
+                if (state[i] > state[j]) and state[i] != 0 and state[j] != 0:
+                    inversion += 1
+
+        return inversion % 2 == 0
+
+    # TODO
+    def h(self, node):
+        """ Return the heuristic value for a given state."""
 
 # ______________________________________________________________________________
 
@@ -1582,7 +1736,6 @@ def compare_graph_searchers():
 # ALL CODE BELOW WRITTEN FOR COM S 472 Lab 1 Assignment
 # @author Ryan Herren
 
-
 def check_solvable(state):
     n = len(state)
     count = 0
@@ -1656,6 +1809,7 @@ def main(argv):
     for i in initial:
         l1.append(int(i))
     puzzle = EightPuzzle(tuple(l1))
+    puzzle2 = EightPuzzle2(tuple(l1))
 
     # CHOOSE ALGORITHM
     if alg in ("BFS", "bfs"):
@@ -1677,9 +1831,8 @@ def main(argv):
         print(end_state.solution())
         print()
     elif alg in ("H2", "h2"):
-        return "Not yet implemented"
         start_time = time.time()
-        end_state = astar_search(puzzle)
+        end_state = astar_search(puzzle2)
         print("\ntime:", time.time() - start_time)
         print(end_state.solution())
         print()
