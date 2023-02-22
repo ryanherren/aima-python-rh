ac200676d824be0703d2d4063525ab2495cf8ff9
ryanherren <ryanherren48@gmail.com>
2023-02-22 05:16:40 -0600

    rearranged order to prioritize h1

--- tests_472/automatetest_depth24.sh
+++ tests_472/automatetest_depth24.sh
@@ -1,53 +1,5 @@
 #!/bin/bash
 echo "BEGINNING BATCH - DEPTH:24"
-echo "3"
-echo "2"
-echo "1"
-echo "BFS"
-python search.py --fPath 116100.txt --alg bfs
-python search.py --fPath 116135.txt --alg bfs
-python search.py --fPath 116183.txt --alg bfs
-python search.py --fPath 116219.txt --alg bfs
-python search.py --fPath 116253.txt --alg bfs
-python search.py --fPath 116300.txt --alg bfs
-python search.py --fPath 116311.txt --alg bfs
-python search.py --fPath 116367.txt --alg bfs
-python search.py --fPath 116469.txt --alg bfs
-python search.py --fPath 116466.txt --alg bfs
-python search.py --fPath 116486.txt --alg bfs
-python search.py --fPath 116698.txt --alg bfs
-python search.py --fPath 116721.txt --alg bfs
-python search.py --fPath 116741.txt --alg bfs
-python search.py --fPath 116827.txt --alg bfs
-python search.py --fPath 117456.txt --alg bfs
-python search.py --fPath 118041.txt --alg bfs
-python search.py --fPath 118060.txt --alg bfs
-python search.py --fPath 118402.txt --alg bfs
-python search.py --fPath 118427.txt --alg bfs
-echo "IDS"
-echo "3"
-echo "2"
-echo "1"
-python search.py --fPath 116100.txt --alg ids
-python search.py --fPath 116135.txt --alg ids
-python search.py --fPath 116183.txt --alg ids
-python search.py --fPath 116219.txt --alg ids
-python search.py --fPath 116253.txt --alg ids
-python search.py --fPath 116300.txt --alg ids
-python search.py --fPath 116311.txt --alg ids
-python search.py --fPath 116367.txt --alg ids
-python search.py --fPath 116469.txt --alg ids
-python search.py --fPath 116466.txt --alg ids
-python search.py --fPath 116486.txt --alg ids
-python search.py --fPath 116698.txt --alg ids
-python search.py --fPath 116721.txt --alg ids
-python search.py --fPath 116741.txt --alg ids
-python search.py --fPath 116827.txt --alg ids
-python search.py --fPath 117456.txt --alg ids
-python search.py --fPath 118041.txt --alg ids
-python search.py --fPath 118060.txt --alg ids
-python search.py --fPath 118402.txt --alg ids
-python search.py --fPath 118427.txt --alg ids
 echo "h1"
 echo "3"
 echo "2"
@@ -96,3 +48,51 @@ python search.py --fPath 118041.txt --alg h2
 python search.py --fPath 118060.txt --alg h2
 python search.py --fPath 118402.txt --alg h2
 python search.py --fPath 118427.txt --alg h2
+echo "3"
+echo "2"
+echo "1"
+echo "BFS"
+python search.py --fPath 116100.txt --alg bfs
+python search.py --fPath 116135.txt --alg bfs
+python search.py --fPath 116183.txt --alg bfs
+python search.py --fPath 116219.txt --alg bfs
+python search.py --fPath 116253.txt --alg bfs
+python search.py --fPath 116300.txt --alg bfs
+python search.py --fPath 116311.txt --alg bfs
+python search.py --fPath 116367.txt --alg bfs
+python search.py --fPath 116469.txt --alg bfs
+python search.py --fPath 116466.txt --alg bfs
+python search.py --fPath 116486.txt --alg bfs
+python search.py --fPath 116698.txt --alg bfs
+python search.py --fPath 116721.txt --alg bfs
+python search.py --fPath 116741.txt --alg bfs
+python search.py --fPath 116827.txt --alg bfs
+python search.py --fPath 117456.txt --alg bfs
+python search.py --fPath 118041.txt --alg bfs
+python search.py --fPath 118060.txt --alg bfs
+python search.py --fPath 118402.txt --alg bfs
+python search.py --fPath 118427.txt --alg bfs
+echo "IDS"
+echo "3"
+echo "2"
+echo "1"
+python search.py --fPath 116100.txt --alg ids
+python search.py --fPath 116135.txt --alg ids
+python search.py --fPath 116183.txt --alg ids
+python search.py --fPath 116219.txt --alg ids
+python search.py --fPath 116253.txt --alg ids
+python search.py --fPath 116300.txt --alg ids
+python search.py --fPath 116311.txt --alg ids
+python search.py --fPath 116367.txt --alg ids
+python search.py --fPath 116469.txt --alg ids
+python search.py --fPath 116466.txt --alg ids
+python search.py --fPath 116486.txt --alg ids
+python search.py --fPath 116698.txt --alg ids
+python search.py --fPath 116721.txt --alg ids
+python search.py --fPath 116741.txt --alg ids
+python search.py --fPath 116827.txt --alg ids
+python search.py --fPath 117456.txt --alg ids
+python search.py --fPath 118041.txt --alg ids
+python search.py --fPath 118060.txt --alg ids
+python search.py --fPath 118402.txt --alg ids
+python search.py --fPath 118427.txt --alg ids
